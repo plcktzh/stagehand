a254d8330964a2843da27311e2ce2e34bb2d6746
Sascha Neumann <sn@09f.de>
2024-12-02 18:11:41 +0100

    Add Github link to Footer; Update Header CSS

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,3 +1,12 @@
+import { GithubLogo } from '@phosphor-icons/react';
+
 export default function Footer() {
-	return <footer className="site-footer content-padding">Footer</footer>;
+	return (
+		<footer className="site-footer content-padding">
+			<a href="https://github.com/plcktzh/stagehand">
+				<GithubLogo weight="duotone" />
+				Github
+			</a>
+		</footer>
+	);
 }
