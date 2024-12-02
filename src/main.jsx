83a7f098b06ea88ce0d41f552d04a42d05e502bd
Sascha Neumann <sn@09f.de>
2024-12-02 11:08:53 +0100

    Add basic project scaffolding

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Einkommentiren, wenn die Bibliothek benutzt werden soll:
+// import '@animxyz/core';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<h1>React-Projekt 👩🏻‍💻</h1>
+	</React.StrictMode>
+);
