1b82a58bd7f21459f503a578f572b99b961e15f7
Sascha Neumann <sn@09f.de>
2024-12-02 14:15:11 +0100

    Some basic setup; Add vite-plugin-webfont-dl dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Einkommentiren, wenn die Bibliothek benutzt werden soll:
-// import '@animxyz/core';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<h1>React-Projekt 👩🏻‍💻</h1>
+			<header className="site-header content-padding">Header</header>
+			<main className="site-content content-padding">
+				<h1>React-Projekt 👩🏻‍💻</h1>
+			</main>
+			<footer className="site-footer content-padding">Footer</footer>
 	</React.StrictMode>
 );
